d06a9000b8cd6041bda733e2531590770d180a90
Efren Bernal <efrenbernal90@gmail.com>
2022-01-19 16:20:14 -0800

    fixed single points branch

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,7 +4,6 @@ console.log("working");
 // Create the map object with a center and zoom level.
 // let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-
 // Create the map object with a center and zoom level (alternative for multiple layers).
 let map = L.map("mapid", {
     center: [
@@ -19,16 +18,24 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
     maxZoom: 18,
     accessToken: API_KEY
 });
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);  
-console.log("Here is a new map!")
+
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
+//  Add a circle to the map for Los Angeles, California.
+// let marker = L.circle([34.0522, -118.2437], {
+//    radius: 100
+// }).addTo(map);
 
- }).addTo(map);
- 
+//  Add a circle to the map for Los Angeles, California using circleMarker().
+let marker = L.circleMarker([34.0522, -118.2437],{
+    radius: 300,
+    color: 'black',
+    fillColor: 'ffffa1'
+
+}).addTo(map);
+
+
